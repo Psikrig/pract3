1d6411d83cf3b512d2b99ba28a0928aa2d410c89
Psikrig <144986514+Psikrig@users.noreply.github.com>
2023-10-10 14:12:05 +0300

    Update Program.cs

--- pract3/Program.cs
+++ pract3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+using System;
+using System.Runtime.InteropServices;
 
 
     Console.WriteLine("пианина");
@@ -6,80 +7,23 @@ double[] k = new double[12] { 1635, 1732, 1835, 1945, 2060, 2183, 2312, 2450, 25
 while (true)
 {
     ConsoleKeyInfo key = Console.ReadKey();
-    int w = 0;
-    if (key.Key == ConsoleKey.A)
-    {
-        w = 1;
-    }
-    if (key.Key == ConsoleKey.S)
-    {
-        w = 2;
-    }
-    if (key.Key == ConsoleKey.D)
-    {
-        w = 3;
-    }
-    if (key.Key == ConsoleKey.F)
-    {
-        w = 4;
-    }
-    if (key.Key == ConsoleKey.G)
-    {
-        w = 5;
-    }
-    if (key.Key == ConsoleKey.H)
-    {
-        w = 6;
-    }
-    if (key.Key == ConsoleKey.J)
-    {
-        w = 7;
-    }
-    if (key.Key == ConsoleKey.W)
-    {
-        w = 8;
-    }
-    if (key.Key == ConsoleKey.E)
-    {
-        w = 9;
-    }
-    if (key.Key == ConsoleKey.T)
-    {
-        w = 10;
-    }
-    if (key.Key == ConsoleKey.Y)
-    {
-        w = 11;
-    }
-    if (key.Key == ConsoleKey.U)
-    {
-        w = 12;
-    }
-    if (key.Key == ConsoleKey.F1)
-    {
-        w = 13;
-    }
-    if (key.Key == ConsoleKey.F2)
-    {
-        w = 14;
-    }
-    octav(w);
-    klav(k, w);
+    octav(key);
+    klav(k, key);
     if (key.Key == ConsoleKey.Escape)
     { break; }
 }
-static void octav(int w)
+static void octav(ConsoleKeyInfo key)
 {
     double[] octave1 = new double[12] { 1635, 1732, 1835, 1945, 2060, 2183, 2312, 2450, 2596, 2750, 2914, 3087 };
     double[] octave2 = new double[12] { 3270, 3465, 3671, 3889, 4120, 4365, 4625, 4900, 5191, 5500, 5827, 6174 };
 
 
-    if (w==13)
+    if (key.Key == ConsoleKey.F1)
     {
         double[] k = octave1;   
 
     }
-    if (w == 14)
+    if (key.Key == ConsoleKey.F2)
     {
         double[] k = octave2;
     }
@@ -87,56 +31,53 @@ static void octav(int w)
 
     
 }
-static void klav(double[] k,int w)
+static void klav(double[] k, ConsoleKeyInfo key)
 {
- 
-        if (w == 1)
-        {
-            Console.Beep(Convert.ToInt32(k[9]), 400);
-        }
-        if (w == 2)
-        {
-            Console.Beep(Convert.ToInt32(k[11]), 400);
-        }
-        if (w == 3)
-        {
-            Console.Beep(Convert.ToInt32(k[0]), 400);
-        }
-        if (w == 4)
-        {
-            Console.Beep(Convert.ToInt32(k[2]), 400);
-        }
-        if (w == 5)
-        {
-            Console.Beep(Convert.ToInt32(k[4]), 400);
-        }
-        if (w == 6)
-        {
-            Console.Beep(Convert.ToInt32(k[5]), 400);
-        }
-        if (w == 7)
-        {
-            Console.Beep(Convert.ToInt32(k[9]), 400);
-        }
-        if (w == 8)
-        {
-            Console.Beep(Convert.ToInt32(k[10]), 300);
-        }
-        if (w == 9)
-        {
-            Console.Beep(Convert.ToInt32(k[1]), 400);
-        }
-        if (w == 10)
-        {
-            Console.Beep(Convert.ToInt32(k[3]), 400);
-        }
-        if (w == 11)
-        {
-            Console.Beep(Convert.ToInt32(k[6]), 400);
-        }
-        if (w == 12)
-        {
-            Console.Beep(Convert.ToInt32(k[8]), 400);
-        }
-         
-}
+    if (key.Key == ConsoleKey.A)
+    {
+        Console.Beep(Convert.ToInt32(k[9]), 400);
+    }
+    if (key.Key == ConsoleKey.S)
+    {
+        Console.Beep(Convert.ToInt32(k[11]), 400);
+    }
+    if (key.Key == ConsoleKey.D)
+    {
+        Console.Beep(Convert.ToInt32(k[0]), 400);
+    }
+    if (key.Key == ConsoleKey.F)
+    {
+        Console.Beep(Convert.ToInt32(k[2]), 400);
+    }
+    if (key.Key == ConsoleKey.G)
+    {
+        Console.Beep(Convert.ToInt32(k[4]), 400);
+    }
+    if (key.Key == ConsoleKey.H)
+    {
+        Console.Beep(Convert.ToInt32(k[5]), 400);
+    }
+    if (key.Key == ConsoleKey.J)
+    {
+        Console.Beep(Convert.ToInt32(k[9]), 400);
+    }
+    if (key.Key == ConsoleKey.W)
+    {
+        Console.Beep(Convert.ToInt32(k[10]), 400);
+    }
+    if (key.Key == ConsoleKey.E)
+    {
+        Console.Beep(Convert.ToInt32(k[1]), 400);
+    }
+    if (key.Key == ConsoleKey.T)
+    {
+        Console.Beep(Convert.ToInt32(k[1]), 400);
+    }
+    if (key.Key == ConsoleKey.Y)
+    {
+        Console.Beep(Convert.ToInt32(k[6]), 400);
+    }
+    if (key.Key == ConsoleKey.U)
+    {
+        Console.Beep(Convert.ToInt32(k[8]), 400);
+    }
